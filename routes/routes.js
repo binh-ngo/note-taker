f01b2fff521d7fcaa7a4c8188f71b962850ffcb4
Binh Ngo <bnngo97@gmail.com>
2023-01-31 11:43:31 -0800

    fixed delete button functionality

--- routes/routes.js
+++ routes/routes.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const path = require('path');
 
 var uniqid = require('uniqid');
-module.exports = app => {
+module.exports = (app) => {
 
         // API routes
 
         // Get route setup | takes the db.json file and returns all saved notes as JSON
         app.get("/api/notes", (req, res) => {
-            res.sendFile(path.join(__dirname, '../db/db.json'))
+            res.sendFile(path.join(__dirname, '../db/db.json'));
         });
         // Post route setup | receives the users note and adds it to the note db and returns it
         app.post("/api/notes", (req, res) => {
@@ -24,14 +24,15 @@ module.exports = app => {
             userNotes.push(noteObj);
             fs.writeFileSync('db/db.json', JSON.stringify(userNotes));
             res.json(userNotes);
-            return console.log(`Created note: ${userNotes.title}`);
         });
 
-        // Gets a note by ID | Displays information in JSON that have the provided ID
-        app.get("/api/notes/:id", (req, res) => res.json(notes[req.params.id])
-        );
-
-        // View Routes
+        app.delete('/api/notes/:id', (req, res) => {
+            let userNotes = JSON.parse(fs.readFileSync('db/db.json'))
+            let deleteNote = userNotes.filter(item => item.id !== req.params.id);
+            fs.writeFileSync('db/db.json', JSON.stringify(deleteNote));
+            res.json(deleteNote);
+        })
+                    // View Routes
         // Displays notes.html when the user navigates to /notes
         app.get("/notes", (req,res) => res.sendFile(path.join(__dirname, "../public/notes.html")));
 
